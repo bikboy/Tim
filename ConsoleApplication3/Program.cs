26e9568ba31a5c914459a4d8d1758610bb268038
TIM BIKBAEV <tbikbaev@gmail.com>
2015-07-13 11:07:49 +0300

    with edwdevel ws

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -24,36 +24,28 @@ namespace ConsoleApplication3
                     new XAttribute(XNamespace.Xmlns + "soapenv", soapenv),
                     new XElement(soapenv + "Header"),
                     new XElement(soapenv + "Body",
-                    new XElement(cab + "cardinfo", 
-                            new XElement("inn","1"),
-                            new XElement("accountno","1")
-                            /*new XElement("card",null)*/))));
+                    new XElement(cab + "cardinfo",
+                            new XElement("inn", "2044713906"),
+                            /*new XElement("accountno", "44410551")*/
+                            new XElement("card","0082")))));
             XmlDocument doc1 = new XmlDocument();
             doc1.LoadXml(requestXML.ToString());
             Console.WriteLine(requestXML);
 
             //HttpWebRequest request = ConsoleApplication3.Program.CreateWebRequest();
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://d-imb-mwact:8011/USER_SERVICES/cabinet/services/cabinet");
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://d-imb-mwact:8011/USER_SERVICES/cabinet/services/cabinet");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://edwdevel/cabinet/index.php");
+            //Trust all certificates
+            System.Net.ServicePointManager.ServerCertificateValidationCallback =
+                ((sender, certificate, chain, sslPolicyErrors) => true);
             request.Headers.Add(@"Soap:Envelope");
             request.ContentType = "text/xml;charset=\"utf-8\"";
             request.Accept = "text/xml";
             request.Method = "POST";
-            /*XmlDocument soapEnvelopeXml = new XmlDocument();
-            soapEnvelopeXml.LoadXml(@"<?xml version=""1.0"" encoding=""utf-8""?>
-                                      <soapenv:Envelope xmlns:soapenv=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:cab=""http://d-imb-mwact:8011/USER_SERVICES/cabinet/services/cabinet"">
-                                        <soapenv:Header/>
-                                        <soapenv:Body>
-                                            <cab:cardinfo>
-                                                <inn>1</inn>
-                                                <accountno>1</accountno>
-                                                <card></card>
-                                            </cab:cardinfo>
-                                        </soapenv:Body>
-                                      </soapenv:Envelope>");*/
+            
 
             using (Stream stream = request.GetRequestStream())
             {
-                //soapEnvelopeXml.Save(stream);
                 requestXML.Save(stream);
             }
 
@@ -62,8 +54,6 @@ namespace ConsoleApplication3
                 using (StreamReader rd = new StreamReader(response.GetResponseStream()))
                 {
                     string soapResult = rd.ReadToEnd();
-                 
-                  
                     XDocument doc = XDocument.Parse(soapResult);
 
                     //Console.WriteLine(soapEnvelopeXml.ToString());
@@ -72,20 +62,13 @@ namespace ConsoleApplication3
 
                     Console.WriteLine(result);
 
+                    Console.WriteLine(doc);
+
                     Console.ReadLine();
                 }
             }
         }
-        public static HttpWebRequest CreateWebRequest()
-        {
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(@"http://d-imb-mwact:8011/USER_SERVICES/cabinet/services/cabinet");
-            webRequest.Headers.Add(@"Soap:Envelope");
-            webRequest.ContentType = "text/xml;charset=\"utf-8\"";
-            webRequest.Accept = "text/xml";
-            webRequest.Method = "POST";
-            return webRequest;
-            
-        }
+        
         
     }
 }
