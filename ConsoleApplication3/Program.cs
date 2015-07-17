59fb41e064f866f4ef1c49cde4687d76a65aa7fc
TIM BIKBAEV <tbikbaev@gmail.com>
2015-07-17 09:57:17 +0300

    bus ws

--- ConsoleApplication3/Program.cs
+++ ConsoleApplication3/Program.cs
@@ -15,26 +15,29 @@ namespace ConsoleApplication3
 
         public static void Main()
         {
-
+            string inn = "2044713906";
+            string account = "44410551";
+            string card = "0082";
+            string ws_link = "http://d-imb-mwact:8011/USERVICES/proxy-service/dealdata";
             XNamespace soapenv = "http://schemas.xmlsoap.org/soap/envelope/";
-            XNamespace cab = "https://edwdevel/cabinet/";
+            //XNamespace cab = "https://edwdevel/cabinet/";
+            XNamespace cab = ws_link;
             var requestXML = new XDocument(
-                //new XDeclaration("1.0", "utf-8", String.Empty),
-                new XElement(soapenv + "Envelope",
+                    new XElement(soapenv + "Envelope",
                     new XAttribute(XNamespace.Xmlns + "soapenv", soapenv),
                     new XElement(soapenv + "Header"),
                     new XElement(soapenv + "Body",
                     new XElement(cab + "cardinfo",
-                            new XElement("inn", "2044713906"),
+                            new XElement("inn", inn),
                             /*new XElement("accountno", "44410551")*/
-                            new XElement("card","0082")))));
+                            new XElement("card",card)))));
             XmlDocument doc1 = new XmlDocument();
-            doc1.LoadXml(requestXML.ToString());
-            Console.WriteLine(requestXML);
+            //doc1.LoadXml(requestXML.ToString());
+            //Console.WriteLine(requestXML);
 
             //HttpWebRequest request = ConsoleApplication3.Program.CreateWebRequest();
-            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"http://d-imb-mwact:8011/USER_SERVICES/cabinet/services/cabinet");
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://edwdevel/cabinet/index.php");
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@ws_link);
+            //HttpWebRequest request = (HttpWebRequest)WebRequest.Create(@"https://edwdevel/cabinet/index.php");
             //Trust all certificates
             System.Net.ServicePointManager.ServerCertificateValidationCallback =
                 ((sender, certificate, chain, sslPolicyErrors) => true);
@@ -58,11 +61,46 @@ namespace ConsoleApplication3
 
                     //Console.WriteLine(soapEnvelopeXml.ToString());
 
-                    var result = doc.Descendants("corraccountno").FirstOrDefault().Value;
+                    var ECA = doc.Descendants("ECA").FirstOrDefault().Value;
+                    var ECA_Number = doc.Descendants("ECA_Number").FirstOrDefault().Value;
+                    var credit_check_kab = doc.Descendants("credit_check_kab").FirstOrDefault().Value;
+                    var GiveAllowDate1 = doc.Descendants("GiveAllowDate1").FirstOrDefault().Value;
+                    var firstrepaydate = doc.Descendants("firstrepaydate").FirstOrDefault().Value;
+                    var Requisites_Check = doc.Descendants("Requisites_Check").FirstOrDefault().Value;
+                    var dealstate = doc.Descendants("dealstate").FirstOrDefault().Value;
+                    var Credit_Type = doc.Descendants("Credit_Type").FirstOrDefault().Value;
+                    var Next_Payment = doc.Descendants("Next_Payment").FirstOrDefault().Value;
+                    var Next_Payment_Date = doc.Descendants("Next_Payment_Date").FirstOrDefault().Value;
+                    var Payment_Interval = doc.Descendants("Payment_Interval").FirstOrDefault().Value;
+                    var Appeal_Date = doc.Descendants("Appeal_Date").FirstOrDefault().Value;
+                    var EDPROU = doc.Descendants("EDPROU").FirstOrDefault().Value;
+                    var Fullrepayment = doc.Descendants("Fullrepayment").FirstOrDefault().Value;
+                    var dealno = doc.Descendants("dealno").FirstOrDefault().Value;
+                    var corraccountno = doc.Descendants("corraccountno").FirstOrDefault().Value;
+                    var deal_date = doc.Descendants("deal_date").FirstOrDefault().Value;
+                    var errorCode = doc.Descendants("errorCode").FirstOrDefault().Value;
 
-                    Console.WriteLine(result);
+                    Console.WriteLine("ECA = " + ECA);
+                    Console.WriteLine("ECA_Number = " + ECA_Number);
+                    Console.WriteLine("credit_check_kab = " + credit_check_kab);
+                    Console.WriteLine("GiveAllowDate1 = " + GiveAllowDate1);
+                    Console.WriteLine("firstrepaydate = " + firstrepaydate);
+                    Console.WriteLine("Requisites_Check = " + Requisites_Check);
+                    Console.WriteLine("dealstate = " + dealstate);
+                    Console.WriteLine("Credit_Type = " + Credit_Type);
+                    Console.WriteLine("Next_Payment = " + Next_Payment);
+                    Console.WriteLine("Next_Payment_Date = " + Next_Payment_Date);
+                    Console.WriteLine("Payment_Interval = " + Payment_Interval);
+                    Console.WriteLine("Appeal_Date = " + Appeal_Date);
+                    Console.WriteLine("EDPROU = " + EDPROU);
+                    Console.WriteLine("Fullrepayment = " + Fullrepayment);
+                    Console.WriteLine("dealno = " + dealno);
+                    Console.WriteLine("corraccountno = " + corraccountno);
+                    Console.WriteLine("deal_date = " + deal_date);
+                    Console.WriteLine("errorCode = " + errorCode);
 
-                    Console.WriteLine(doc);
+
+                    //Console.WriteLine(doc);
 
                     Console.ReadLine();
                 }
